a95c850a9e2a035451d91885fa7ed16ceb9e5ace
MariaFernandaCintra <mariafernandaccintra@gmail.com>
2025-04-23 11:15:18 -0300

    modal com confirmação de exclusão

--- src/pages/listUsers.jsx
+++ src/pages/listUsers.jsx
@@ -10,31 +10,40 @@ import TableRow from "@mui/material/TableRow";
 import TableCell from "@mui/material/TableCell";
 import Paper from "@mui/material/Paper";
 import api from "../axios/axios";
-import { Button, IconButton, Alert, Snackbar } from "@mui/material";
-
-import DeleteIcon from "@mui/icons-material/Delete"
+import { Button, IconButton, Alert, Snackbar} from "@mui/material";
+// import { DeleteOutlineIcon } from "@mui/icons-material/DeleteOutline";
+import DeleteIcon from "@mui/icons-material/Delete";
 import { Link, useNavigate } from "react-router-dom";
+import ConfirmDelete from "../components/ConfirmDelete";
 
 function listUsers() {
   const [users, setUsers] = useState([]);
   const [alert, setAlert] = useState({
-    //visibilidade
+    // Visibilidade (false = oculta; true = visível)
     open: false,
     //nível do alerta (sucess, error, warning, etc)
-    severity: "",
+    severity:"",
     message:""
   });
 
-  //função para exibir o alerta 
+  // função para exibir o alerta
   const showAlert = (severity, message) => {
     setAlert({open: true, severity, message})
   };
-
-  //Fechar o alerta
   const handleCloseAlert = () => {
     setAlert({...alert, open: false})
   };
+  
   const navigate = useNavigate();
+
+  const [userToDelete, setUserToDelete] = useState("");
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const openDeleteModal = (id, name) => {
+    setUserToDelete({ id: id, name: name});
+    setModalOpen(true);
+  }
+  
   async function getUsers() {
     // Chamada da Api
     await api.getUsers().then(
@@ -48,14 +57,16 @@ function listUsers() {
     );
   }
 
-  async function deleteUser(id){
+  async function deleteUser(){
     try{
-      await api.deleteUser(id);
+      await api.deleteUser(userToDelete.id);
       await getUsers();
-      showAlert("success", "Usuário apagado com sucesso");
+      showAlert("success", "Usuário Deletado com Sucesso!");
+      setModalOpen(false);
     }catch(error){
-      console.log("erro ao deletar usuario", error);
-      showAlert("error", error.response.data.error);
+      console.log("Erro ao deletar usuário", error)
+      showAlert("error", "erro ao deletar usuário");
+      setModalOpen(false);
     }
   }
 
@@ -65,9 +76,8 @@ function listUsers() {
         <TableCell align="center">{user.name}</TableCell>
         <TableCell align="center">{user.email}</TableCell>
         <TableCell align="center">{user.cpf}</TableCell>
-
         <TableCell align="center">
-          <IconButton onClick={() => deleteUser(user.id_usuario)}>
+          <IconButton onClick={()=> openDeleteModal(user.id_usuario, user.name)}>
             <DeleteIcon color="error"/>
           </IconButton>
         </TableCell>
@@ -92,12 +102,20 @@ function listUsers() {
   return (
     <div>
       <Snackbar open={alert.open} autoHideDuration={3000} onClose={handleCloseAlert} anchorOrigin={{vertical:"top", horizontal:"center"}}>
-        <Alert onClose={handleCloseAlert} severity={alert.severity} sx={{width: "100%"}}>
+        <Alert onClose={handleCloseAlert} severity={alert.severity} sx={{width:"100%"}}>
           {alert.message}
         </Alert>
       </Snackbar>
+
+      <ConfirmDelete
+      open={modalOpen}
+      userName={userToDelete.name}
+      onConfirm={deleteUser}
+      onClose={()=>setModalOpen(false)}
+      />
+ 
       {users.length === 0 ? (
-        <p>Carregando Usuários</p>
+        <p>Carregando Usuários...</p>
       ) : (
         <div>
           <h5>Lista de usuários</h5>
@@ -110,12 +128,22 @@ function listUsers() {
                   <TableCell align="center">Nome</TableCell>
                   <TableCell align="center">Email</TableCell>
                   <TableCell align="center">CPF</TableCell>
-                  <TableCell align="center">AÇÕES</TableCell>
+                  <TableCell align="center">Ações</TableCell>
                 </TableRow>
               </TableHead>
               <TableBody>{listUsers}</TableBody>
             </Table>
           </TableContainer>
+          <Button type="submit"
+            fullWidth
+            variant="contained"
+            sx={{
+              mt: 3,
+              mb: 2,
+              backgroundColor: "green",
+            }}>
+            <Link to="/evento">Lista de Eventos</Link>
+          </Button>
           <Button fullWidth variant="contained" onClick={logout}>
             SAIR
           </Button>
